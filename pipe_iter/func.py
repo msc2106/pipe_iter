82344c0d7ca60b4e2230aec76ad101ee2ae56af6
Mark Cohen <mark.simon.cohen@gmail.com>
2025-02-26 23:27:32 +0800

    star_func plus documenting other funcs

--- pipe_iter/func.py
+++ pipe_iter/func.py
@@ -1,10 +1,16 @@
-from typing import Callable, Any
+from collections.abc import Iterable, Callable, Mapping
+from typing import Any
 
-def star_func(fn: Callable[..., Any]):
-    ...
+def doublestar_func(fn: Callable[..., Any], convert=True):
+    '''Wraps `fn` to unpack mapping arguments. With `convert=True`, the default, tries to convert the argument to a `dict` (e.g. collections of duples).'''
+    return fn
 
-def doublestar_func(fn: Callable[..., Any]):
-    ...
+def fallible_func(fn: Callable[[Any], Any], fail_value: Any | None = None):
+    '''Wraps `fn` to catch exceptions and return `fail_value`.'''
+    return fn
 
-def fallible_func(fn: Callable[[Any], Any]):
-    ...
+def star_func(fn: Callable[..., Any], strict=True):
+    '''Wraps `fn` to unpack iterable single arguments. With `strict=True`, the default, follows behavior of `itertools.starmap` by raising `TypeError` if non-iterable arguments is passed. With `strict=False`, non-iterable arguments are passed as is.'''
+    def new_fn(arg: Iterable):
+        return fn(*arg) if strict or isinstance(arg, Iterable) else fn(arg)
+    return new_fn
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,4 +1,5 @@
-from pipe_iter import star_func, doublestar_func, fallible_func
+from pipe_iter import star_func, doublestar_func, fallible_func, Iter
+from pytest import raises
 
 def test_doublestarfunc():
     ...
@@ -10,10 +11,15 @@ def test_falliblefunc():
     ...
 
 def test_starfunc():
-    ...
+    r = range(10)
+    product = Iter.zipped(r, r).map(star_func(lambda x, y: x * y)).collect(list)
+    assert product == [x**2 for x in r]
 
 def test_starfunc_invalid():
-    ...
+    r = range(10)
+    with raises(TypeError):
+        _ = Iter(r).map(star_func(lambda x: x)).collect(list)
+    list(r) == Iter(r).map(star_func(lambda x: x, strict=False)).collect(list)
 
 def test_stardoublestar():
     ...
